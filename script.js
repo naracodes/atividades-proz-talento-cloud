4f28d27f585afb0ef398c56112ca66a77091d6cc
Edinara Alencar <116104559+naracodes@users.noreply.github.com>
2024-04-13 17:50:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ function imprimirMensagem() {
 imprimirMensagem();
 imprimirMensagem();
 imprimirMensagem();
+
+//Capturando elementos
+let h1 = document.getElementById("titulo");
+let ul = document.querySelector('ul');
+let a = document.querySelector('a');
+let ol = document.getElementById("lista-ordenada");
+
+h1.innerText = "Bem Vindos";
+a.innerText = "Site da Proz";
+ul.innerHTML = ` 
+<li>Cursos</li>
+<li>Notas</li>
+<li>Aulas</li>
+`;
+ol.innerHTML = ` 
+<li>Cursos</li>
+<li>Notas</li>
+<li>Aulas</li>
+`;
